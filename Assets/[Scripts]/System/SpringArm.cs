aa21142f5d9ea8bd767fe467e397dd3fe8961452
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-03-29 14:57:11 -0400

    Follow Camera is implemented

---
+++ Assets/[Scripts]/System/SpringArm.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringArm : MonoBehaviour
+{
+    public Transform Target;
+
+    public Vector3 OffsetFromTarget;
+
+    public float LagFactor;
+
+    private Vector3 _velocity = Vector3.zero;
+
+
+    void LateUpdate()
+    {
+        Vector3 DestinationPosition = Target.position + OffsetFromTarget;
+        //transform.position = Vector3.Lerp(transform.position, Destination,  Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, DestinationPosition, ref _velocity, LagFactor);
+    }
+}
